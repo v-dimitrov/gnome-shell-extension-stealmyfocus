a6623aa7bbbc85a70ca138dd3844a4b0c27c6700
Dominik Zogg <dominik.zogg@hotmail.com>
2023-10-11 21:28:05 +0200

    gnome 45 support

--- extension.js
+++ extension.js
@@ -1,36 +1,14 @@
-const Main = imports.ui.main;
-const WindowAttentionHandler = imports.ui.windowAttentionHandler;
-const Shell = imports.gi.Shell;
-const Lang = imports.lang;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-function StealMyFocus() {
-    this._init();
-}
+export default class FocusMyWindow extends Extension {
+    enable() {
+        this._handlerid = global.display.connect('window-demands-attention', function (display, window) {
+            Main.activateWindow(window);
+        });
+    }
 
-StealMyFocus.prototype = {
-    _init : function() {
-        this._tracker = Shell.WindowTracker.get_default();
-        this._handlerid = global.display.connect('window-demands-attention', Lang.bind(this, this._onWindowDemandsAttention));
-    },
-
-    _onWindowDemandsAttention: function(display, window) {
-        Main.activateWindow(window);
-    },
-
-    destroy: function () {
+    disable() {
         global.display.disconnect(this._handlerid);
     }
-}
-
-let stealmyfocus;
-
-function init() {
-}
-
-function enable() {
-    stealmyfocus = new StealMyFocus();
-}
-
-function disable() {
-    stealmyfocus.destroy();
 }
